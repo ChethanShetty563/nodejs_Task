c432b132437e6ef46fdd040bdf971ae27535aebc
ChethanShetty563 <chethan563@gmail.com>
2022-12-23 08:01:09 +0530

    Added the middlewares

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { StockTransaction } from './service/transactionService';
 
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  let sku = req.query.sku;
+  console.log(sku);
+  StockTransaction(sku as string)
+    .then((data) => {
+      res.send(data).status(200);
+    })
+    .catch((e) => {
+      res.send({ error: 'No SKU', message: e }).status(400);
+    });
 });
 
 app.listen(3000, () => {
