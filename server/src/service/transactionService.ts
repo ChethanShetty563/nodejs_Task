1f28505fb373bfb4d45436bb655fc130b27f5460
ChethanShetty563 <chethan563@gmail.com>
2022-12-23 08:03:49 +0530

    Implemented the business logic for the transaction service

--- server/src/service/transactionService.ts
+++ server/src/service/transactionService.ts
@@ -1,4 +1,7 @@
+import { getStocksBySKU } from '../data/stocks';
 import { IStock } from 'src/interface/iStock';
+import { getTransactionsBySKU } from '../data/transactions';
+import { TransactionType } from '../enum/transactionType';
 
 /**
  *
@@ -6,5 +9,29 @@ import { IStock } from 'src/interface/iStock';
  * @returns object which contains the sku and quantity of stock after transaction
  */
 export function StockTransaction(sku: string): Promise<IStock> {
-  return null;
+  let initialStock = getStocksBySKU(sku);
+  let transactionStock = getTransactionsBySKU(sku);
+
+  if (!initialStock && transactionStock.length == 0) {
+    return Promise.reject('SKU is not present in stocks/transaction');
+  }
+
+  const finalStockQty: IStock = {
+    sku: sku,
+    stock: initialStock ? initialStock.stock : 0,
+  };
+
+  transactionStock.forEach((item) => {
+    if (item.type === TransactionType.ORDER) {
+      finalStockQty.stock -= item.qty;
+    } else if (item.type == TransactionType.REFUND) {
+      finalStockQty.stock += item.qty;
+    }
+  });
+
+  if (finalStockQty.stock <= 0) {
+    return Promise.reject('SKU NOT FOUND');
+  }
+
+  return Promise.resolve(finalStockQty);
 }
