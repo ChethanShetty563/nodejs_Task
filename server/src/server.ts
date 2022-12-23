e08c83b9ea243b039cff53a976c65c78a526be12
ChethanShetty563 <chethan563@gmail.com>
2022-12-23 16:34:19 +0530

    Adding the comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,16 @@
+import cors from 'cors';
 import express from 'express';
-import { StockTransaction } from './service/transactionService';
+import stockApi from './controller/stockApi';
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use('/', stockApi);
 
-app.get('/', (req, res) => {
-  let sku = req.query.sku;
-  console.log(sku);
-  StockTransaction(sku as string)
-    .then((data) => {
-      res.send(data).status(200);
-    })
-    .catch((e) => {
-      res.send({ error: 'No SKU', message: e }).status(400);
-    });
-});
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+// Server listening on 3002 port
+app.listen(3002, () => {
+  console.log('Server listening on port 3002');
 });
